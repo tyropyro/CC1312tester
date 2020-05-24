defa43e44309304f756a23ff9eea6cfcd7288f22
tyropyro <65506237+tyropyro@users.noreply.github.com>
2020-05-23 21:11:28 -0400

    version 2 of the main.rs
    version 2

---
+++ main.rs version2.rs
@@ -0,0 +1,62 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate cortex_m_rt as rt;
+
+use cc1312::Peripherals;
+use rt::entry;
+
+// use `main` as the entry point of this application
+// `main` is not allowed to return
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+        // application logic
+    let gpio = peripherals.GPIO; //calling GPIO
+
+    let led1 = peripherals
+    .GPIO.dout7_4.read().dio6().bits(); //calling LED
+
+    let led2 = peripherals
+    .GPIO.dout7_4.read().dio7().bits(); //calling LED
+
+    let read1 = peripherals.GPIO.evflags31_0
+    .read().dio6();// event reg. for LED
+
+    let read2 = peripherals.GPIO.evflags31_0
+    .read().dio7();// event reg. for LED
+    
+   
+
+    loop{
+        // let seconds = aon.sec.read().bits();
+        // let subseconds = aon.subsec.read().bits(); 
+        
+        // if seconds >= 3{
+        //     let x = 0;
+        //     aon.ctl.modify(| r, w|{
+        //         w.reset().set_bit()
+        //     });
+        //     while aon.ctl.read().reset().bit_is_set(){
+        //         continue;
+          //  }
+        if read1.bit_is_set(){
+            !led1;
+        }
+
+        if read2.bit_is_set(){
+            !led2;
+        }
+
+        
+        continue;
+    }
+    
+
+    loop {
+        continue;
+    }
+}
+
